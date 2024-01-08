92a0a84e8a20da348b6cca547a351be93b064996
siddharthbahl <bahlsiddharth123@gmail.com>
2024-01-08 20:37:15 +0000

    Adding Scanner functionality

--- src/main/java/com/compilers/lox/Lox.java
+++ src/main/java/com/compilers/lox/Lox.java
@@ -9,9 +9,11 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
+
+import com.compilers.lox.Scanner;
 
 public class Lox {
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1){
             System.out.println("Usage: Lox <file_path>");
@@ -27,6 +29,8 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        if (hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException{
@@ -38,6 +42,7 @@ public class Lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+            hadError = false;
         }
     }
 
@@ -48,5 +53,14 @@ public class Lox {
         for (Token token : tokens){
             System.out.println(token);
         }
+    }
+
+    static void error(int line, String message){
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message){
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
