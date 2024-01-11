e4d468709ed9fd2ef4b94286737404ff07e2f3d3
siddharthbahl <bahlsiddharth123@gmail.com>
2024-01-11 12:32:28 +0000

    Adding scanner functionality to deal with keywords and identifiers

--- src/main/java/com/compilers/lox/Scanner.java
+++ src/main/java/com/compilers/lox/Scanner.java
@@ -17,6 +17,27 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+    private static final Map<String, TokenType> keywords;
+
+    static{
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -66,8 +87,13 @@ public class Scanner {
             case '"': string(); break;
 
             default:
-                Lox.error(line, "Unexpected character starting with" + c);
-                break;
+                if (isDigit(c)){
+                    number();
+                } else if(isAlpha(c)){
+                    identifier();
+                } else{
+                    Lox.error(line, "Unexpected character starting with" + c);
+                }
         }
     }
 
@@ -115,5 +141,45 @@ public class Scanner {
         }
 
         addToken(STRING, source.substring(start + 1, current - 1));
+    }
+
+    private boolean isDigit(char c){
+        return c >= '0' && c <= '9';
+    }
+
+    private void identifier(){
+        while (isAlphaNumeric(peek())) advance();
+
+        TokenType tokenType = keywords.get(source.substring(start, current));
+        if (tokenType == null) tokenType = IDENTIFIER;
+
+        addToken(tokenType);
+    }
+
+    private boolean isAlphaNumeric(char c){
+        return (isAlpha(c) || isDigit(c));
+    }
+
+    private void number(){
+        while (isDigit(peek())) advance();
+
+        if (peek() == '.' && isDigit(peekNext())){
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private char peekNext(){
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c){
+        return ((c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                (c == '_'));
     }
 }
